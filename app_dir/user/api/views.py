d56848d15291f2932c5453aa5ddb988f052d6241
pusku <puskuuuu@gmail.com>
2020-02-04 01:41:09 +0600

    role based access specified to all user api

--- app_dir/user/api/views.py
+++ app_dir/user/api/views.py
@@ -16,8 +16,38 @@ from rest_framework.decorators import action
 from rest_framework.parsers import FormParser, MultiPartParser, JSONParser, FileUploadParser
 from django.http import HttpResponse
 
+class IsAdminUser(permissions.BasePermission):
+    """
+    Allows access only to admin users.
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
+
+class IsUser(permissions.BasePermission):
+    """
+    Allows access only to library member.
+    """
+    def has_permission(self, request, view):
+        if request.user:
+            if not request.user.is_staff:
+                return True
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.id == request.user.id
+
 class UserListAPIView(ListAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
     pagination_class = PostLimitOffsetPagination
 
@@ -41,7 +71,7 @@ class UserListAPIView(ListAPIView):
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminUser]
     queryset = User.objects.all()
 
 
@@ -50,33 +80,32 @@ class UserDetailAPIView(RetrieveAPIView):
     serializer_class = UserSerializer
     parser_classes = (JSONParser, FormParser, MultiPartParser)
 
-
 class UserDeleteAPIView(DestroyAPIView):
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
 
 
 class UpdateAPIView(RetrieveUpdateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminUser,]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
 
-class ProfileView(UpdateAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = ProfileSerializer
+class ProfileView(RetrieveUpdateAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = UserProfile.objects.all()
+    serializer_class = ProfileSerializer
 
 class MemberRegistrationAPIView(CreateAPIView):
     serializer_class = MemberRegistrationSerializer
-    permission_classes = []
+    permission_classes = [IsUser]
     queryset = User.objects.all()
 
 class MemberProfileUpdateAPIView(RetrieveUpdateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = User.objects.all()
     serializer_class = MemberRegistrationSerializer
 

b29b632bd4b208b7a03bc08c93c4efb6e7c9d80c
pusku <puskuuuu@gmail.com>
2020-02-04 00:29:53 +0600

    UserProfile model added for implementing User Profile Image Uploading

--- app_dir/user/models.py
+++ app_dir/user/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+class UserProfile(models.Model):
+    User = get_user_model()
+    user   = models.OneToOneField(User,on_delete=models.CASCADE,)
+    image = models.ImageField(upload_to="accounts/images/", null=True, blank=True)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=False,
+        null=True)
